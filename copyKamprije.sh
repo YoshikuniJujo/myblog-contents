9934e43efa4cc43937cdbea9d1a634c6791c9f1f
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-09-01 10:51:56 +0900

    automate copy and update kamprije

---
+++ copyKamprije.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp ~/phd01_black/daily/study/lojban/ciska/kamprije.txt homepage/third/lojban/ciska
+cp ~/phd01_black/daily/study/lojban/ciska/kamprije_text.txt homepage/third/lojban/ciska
+cp ~/phd01_black/daily/study/lojban/ciska/kamprije.html homepage/third/lojban/ciska
+cp ~/phd01_black/daily/study/lojban/ciska/kamprije_with_note.html homepage/third/lojban/ciska
+cp ~/phd01_black/daily/study/lojban/ciska/kamprije_text.html homepage/third/lojban/ciska
---
+++ updateKamprije.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+yjftp-ni put homepage/third/lojban/ciska/kamprije.txt ftp37.nifty.com/homepage/third/lojban/ciska hc751476
+yjftp-ni put homepage/third/lojban/ciska/kamprije_text.txt ftp37.nifty.com/homepage/third/lojban/ciska hc751476
+yjftp-ni put homepage/third/lojban/ciska/kamprije.html ftp37.nifty.com/homepage/third/lojban/ciska hc751476
+yjftp-ni put homepage/third/lojban/ciska/kamprije_with_note.html ftp37.nifty.com/homepage/third/lojban/ciska hc751476
+yjftp-ni put homepage/third/lojban/ciska/kamprije_text.html ftp37.nifty.com/homepage/third/lojban/ciska hc751476
